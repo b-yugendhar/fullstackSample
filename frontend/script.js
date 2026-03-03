0126d6082d2d53590eff894a8726d2156898982b
Yugendhar <byugendharnaidu@gmail.com>
2026-03-03 14:59:31 +0530

    jdh

--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,20 @@
-const f = document.getElementById("f")
-const r = document.getElementById("r")
+const f = document.getElementById("f");
+const r = document.getElementById("r");
 
 f.addEventListener("submit", async (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const n = document.getElementById("n").value
-    const m = document.getElementById("m").value
+  const n = document.getElementById("n").value;
+  const m = document.getElementById("m").value;
 
-    const d = await fetch("https://fullstacksamplebackend.onrender.com", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ n, m })
-    })
+  const d = await fetch("https://fullstacksamplebackend.onrender.com/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ n, m }),
+  });
 
-    const j = await d.json()
-    r.innerText = j.message
-})
+  const j = await d.json();
+  r.innerText = j.message;
+});
